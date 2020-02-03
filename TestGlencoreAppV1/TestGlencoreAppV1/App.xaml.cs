16e7890a0d48108ece918a325e1417a1fbad3de6
Bruno Targhetta <bruno_targhetta@hotmail.com>
2020-02-03 10:44:26 -0300

    Add project files.

---
+++ TestGlencoreAppV1/TestGlencoreAppV1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TestGlencoreAppV1.Services;
+using TestGlencoreAppV1.Views;
+
+namespace TestGlencoreAppV1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
